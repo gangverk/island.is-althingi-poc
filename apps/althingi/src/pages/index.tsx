d8a701273608bd64c30bc8d86c7e81b802d02b00
Adam Baumruk <ah.baumruk@gmail.com>
2025-05-08 13:43:40 +0000

    Make video work use https stream

--- apps/althingi/src/pages/index.tsx
+++ apps/althingi/src/pages/index.tsx
@@ -1,15 +1,15 @@
-import { Box, Button, Inline, Stack } from '@island.is/island-ui/core'
-import Link from 'next/link'
+import { Text, Page, LinkV2, Box } from '@island.is/island-ui/core'
+import { LinkToAlthingi } from 'apps/althingi/utils/mockData'
 
 export function Index() {
   return (
-    <Box paddingY={4} paddingX={2}>
-      <Button>
-        <Stack space={2}>
-          <Link href="/althingi">Fara á Alþingi</Link>
-        </Stack>
-      </Button>
-    </Box>
+    <Page>
+      <Box padding={4}>
+        <LinkV2 href={LinkToAlthingi}>
+          <Text color="blue400">Fara á Althingi</Text>
+        </LinkV2>
+      </Box>
+    </Page>
   )
 }
 
